ad774951604413d4f240f2968756a46f3daf7c49
Assafm <assafe.cohen@gmail.com>
2020-03-10 02:07:46 +0200

    added google fonts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
  body{
-  background-color: blue;
+  font-family: 'Open Sans', sans-serif;
+ }
+ h1,h2,h3{
+  font-family: 'Righteous', cursive;
  }
 `;
 
 function App() {
   return (
     <Fragment>
+      <h1>Hello</h1>
       <GlobalStyle />
       <div>Hello</div>
     </Fragment>
@@ -17,4 +21,3 @@ function App() {
 }
 
 export default App;
-g;
